d2fbddec470c664b5dbf0f2bb28ed238e667cd5d
Ahmad Ali <ahmadali.afzal007@gmail.com>
2025-01-21 16:15:41 +0500

    Add project files.

---
+++ AzureOpenAI_Bots/Models/Message.cs
@@ -0,0 +1,17 @@
+﻿namespace AzureOpenAI_Bots.Models
+{
+    public class Message
+    {
+        public DateTime TimeStamp { get; set; }
+        public bool IsRequest { get; set; }
+        public string? Body { get; set; }
+
+        public Message(bool IsRequest, string Body)
+        {
+            TimeStamp = DateTime.Now;
+            this.IsRequest = IsRequest;
+            this.Body = Body;
+        }
+
+    }
+}
---
+++ AzureOpenAI_Bots/Services/ChatService.cs
@@ -0,0 +1,71 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using Azure.Identity;
+using AzureOpenAI_Bots.Models;
+using OpenAI.Chat;
+using static System.Environment;
+
+
+namespace AzureOpenAI_Bots.Services
+{
+    public class ChatService
+    {
+
+
+        public async Task<Message> GetMessage(List<Message> messageChain)
+        {
+            try
+            {
+                // Retrieve the OpenAI endpoint from environment variables
+                string endpoint = GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
+
+
+                // Use DefaultAzureCredential for Entra ID authentication
+                var credential = new DefaultAzureCredential();
+
+                // Initialize the AzureOpenAIClient
+                var azureClient = new AzureOpenAIClient(new Uri(endpoint), credential);
+
+                // Initialize the ChatClient with the specified deployment name
+                ChatClient chatClient = azureClient.GetChatClient("gpt-35-turbo-16k");
+
+                // Create a list of chat messages
+                var messages = new List<ChatMessage>
+                {
+                    new SystemChatMessage("You are an AI assistant that helps people find information."),
+
+                };
+                foreach (var message in messageChain)
+                {
+                    messages.Add(new UserChatMessage(message.Body));
+                }
+
+                // Create chat completion options
+                var options = new ChatCompletionOptions
+                {
+                    Temperature = (float)0.7,
+                    MaxOutputTokenCount = 800,
+
+                    FrequencyPenalty = 0,
+                    PresencePenalty = 0,
+                };
+
+                // Create the chat completion request
+                ChatCompletion completion = await chatClient.CompleteChatAsync(messages, options);
+
+                int x = 0;
+              
+                return new Message(false, completion.Content.ToString());
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return new Message(true, $"An error occurred: {ex.Message}");
+            }
+        }
+
+
+    }
+
+}
